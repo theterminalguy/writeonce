2f9e72b3f7a9f789dda0706e1bdcd7d2997d6d03
theterminalguy <damiansimonpeter@gmail.com>
2022-12-28 09:30:49 -0500

    Add classes to button

--- src/components/editors/vanilla/plugins/FloatingToolBarPlugin/index.tsx
+++ src/components/editors/vanilla/plugins/FloatingToolBarPlugin/index.tsx
@@ -1,18 +1,24 @@
-import './index.css';
+import "./index.css";
 export default function FloatingToolBarPlugin() {
   return (
     <div className="floating-toolbar">
-      <button type="button" className="floating-toolbar-button">
+      <button
+        type="button"
+        className="floating-toolbar-button btn-make-placeholder"
+        title="Make placeholder"
+      >{`{x}`}</button>
+      <button type="button" className="floating-toolbar-button btn-make-bold" title="Bold">
         B
       </button>
-      <button type="button" className="floating-toolbar-button">
+      <button type="button" className="floating-toolbar-button btn-make-italic" title="Italic">
         I
       </button>
-      <button type="button" className="floating-toolbar-button">
+      <button
+        type="button"
+        className="floating-toolbar-button btn-make-underline"
+        title="Underline"
+      >
         U
-      </button>
-      <button type="button" className="floating-toolbar-button">
-        S
       </button>
     </div>
   );
