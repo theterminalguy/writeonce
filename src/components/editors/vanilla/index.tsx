a8afeeb8ab1d9386c1766a828e61d02f58e4abf6
theterminalguy <damiansimonpeter@gmail.com>
2022-12-28 09:05:52 -0500

    Added editor container

---
+++ src/components/editors/vanilla/index.tsx
@@ -0,0 +1,23 @@
+import './index.css';
+
+export default function Editor() {
+  return (
+    <div className="editor-container">
+      <h1 contentEditable="true">New Template</h1>
+      <div contentEditable="true" className="editor">
+        Vestibulum vel orci hendrerit ligula pharetra volutpat et sed dui.
+        Phasellus vitae feugiat dolor. Mauris eleifend neque ac iaculis aliquet.
+        Nunc malesuada nisi in dictum tristique. Vestibulum mauris eros, varius
+        sed faucibus sed, pellentesque et nunc. Curabitur ultricies blandit
+        urna. Pellentesque ut augue mollis, lacinia dui non, rutrum justo. Nunc
+        et odio dapibus, blandit leo eget, aliquet urna. Etiam lorem dolor,
+        vehicula a purus in, fermentum congue diam. Integer vel urna nec turpis
+        posuere tempor eu lacinia purus. Praesent mattis viverra lacus bibendum
+        fringilla. Nulla commodo posuere ante, at cursus est rhoncus quis. In
+        iaculis viverra neque in blandit. Pellentesque eleifend arcu diam, sed
+        sodales ligula pharetra at. Morbi ac nisl adipiscing sem interdum
+        convallis.
+      </div>
+    </div>
+  );
+}
