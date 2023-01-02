d31f7b57ba252350d9d0ae72c751c78e3b8aab33
theterminalguy <damiansimonpeter@gmail.com>
2023-01-01 20:23:30 -0500

    Extract sidepanle component

--- src/components/editors/vanilla/components/PlaceholderSidePanel/PlaceholderItem.tsx
+++ src/components/editors/vanilla/components/PlaceholderSidePanel/PlaceholderItem.tsx
@@ -1,42 +1,45 @@
-import './PlaceholderItem.css';
+import "./PlaceholderItem.css";
 
+export default class PlaceholderItem {
+  render(): JSX.Element {
+    return (
+      <div className="vanilla__placeholder-item ">
+        <div className="vanilla__placeholder-item-header">
+          <span className="vanilla__placeholder-item-name">
+            Placeholder name
+          </span>
+          <button type="button" className="vanilla__placeholder-item-delete">
+            X
+          </button>
+        </div>
+        <div className="vanilla__placeholder-field">
+          <label htmlFor="placeholder-type">Type</label>
+          <select name="placeholder-type" id="placeholder-type">
+            <option value="text">Text</option>
+          </select>
+        </div>
 
-export default function PlaceholderItem() {
-  return (
-    <div className="vanilla__placeholder-item ">
-      <div className="vanilla__placeholder-item-header">
-        <span className="vanilla__placeholder-item-name">Placeholder name</span>
-        <button type="button" className="vanilla__placeholder-item-delete">
-          X
-        </button>
-      </div>
-      <div className="vanilla__placeholder-field">
-        <label htmlFor="placeholder-type">Type</label>
-        <select name="placeholder-type" id="placeholder-type">
-          <option value="text">Text</option>
-        </select>
-      </div>
+        <div className="vanilla__placeholder-field">
+          <label htmlFor="default-value">Default value</label>
+          <input type="text" id="default-value" name="default-value" />
+        </div>
 
-      <div className="vanilla__placeholder-field">
-        <label htmlFor="default-value">Default value</label>
-        <input type="text" id="default-value" name="default-value" />
-      </div>
+        <div className="vanilla__placeholder-field">
+          <label htmlFor="required">Required</label>
+          <input type="checkbox" id="required" name="required" />
+        </div>
 
-      <div className="vanilla__placeholder-field">
-        <label htmlFor="required">Required</label>
-        <input type="checkbox" id="required" name="required" />
+        <div className="vanilla__placeholder-field">
+          <label htmlFor="description">Add description (optional)</label>
+          <textarea
+            id="description"
+            name="description"
+            cols={26}
+            rows={3}
+            maxLength={50}
+          ></textarea>
+        </div>
       </div>
-
-      <div className="vanilla__placeholder-field">
-        <label htmlFor="description">Add description (optional)</label>
-        <textarea
-          id="description"
-          name="description"
-          cols={26}
-          rows={3}
-          maxLength={50}
-        ></textarea>
-      </div>
-    </div>
-  );
+    );
+  }
 }
--- src/components/editors/vanilla/components/PlaceholderSidePanel/index.tsx
+++ src/components/editors/vanilla/components/PlaceholderSidePanel/index.tsx
@@ -2,11 +2,13 @@ import './index.css'
 import PlaceholderItem from './PlaceholderItem';
 
 export default function PlaceholderSidePanel() {
+  const placeholderItem = new PlaceholderItem();
+
   return (
     <div className="vanilla__placeholder-sidepanel-wrapper">
       <form name="vanilla__placeholder-side-panel">
-        <PlaceholderItem />
-      </form>;
+        {placeholderItem.render()}
+      </form>
     </div>
   );
 }
