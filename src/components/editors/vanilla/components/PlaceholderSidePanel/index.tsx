b72e0d21f7f3700cf901d442bf1ce972f0a37dd1
theterminalguy <damiansimonpeter@gmail.com>
2023-01-04 14:25:15 -0500

    okay

--- src/components/editors/vanilla/components/PlaceholderSidePanel/index.tsx
+++ src/components/editors/vanilla/components/PlaceholderSidePanel/index.tsx
@@ -1,13 +1,8 @@
-import PlaceholderItem from "./PlaceholderItem";
 import "./index.css";
 
 export default function PlaceholderSidePanel() {
   return (
     <div className="vanilla__placeholder-side-panel">
-      <PlaceholderItem 
-        placeholderId="placeholder-1"
-        placeholderName="Placeholder 1"
-      />
     </div>
   );
 }
