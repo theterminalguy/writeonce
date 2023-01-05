3ce19c0b2693c139d5258cde4767c91253e74cbe
theterminalguy <damiansimonpeter@gmail.com>
2023-01-04 20:12:19 -0500

    Add redux dev tool extension to dev environment

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 
 import counterReducer from "./counterSlice";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
+  devTools: devToolsEnhancer({}),
 });
 
 export default rootReducer;
