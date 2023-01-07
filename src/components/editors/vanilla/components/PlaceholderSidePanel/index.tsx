94136099d2408a6b2b339a1d57fb2ed064686560
theterminalguy <damiansimonpeter@gmail.com>
2023-01-06 22:35:42 -0500

    [wrt-14] add event handlers

--- src/components/editors/vanilla/components/PlaceholderSidePanel/index.tsx
+++ src/components/editors/vanilla/components/PlaceholderSidePanel/index.tsx
@@ -9,46 +9,30 @@ export default function PlaceholderSidePanel() {
       const placeholder = customEvent.detail as {
         id: string;
         name: string;
-      }
-      const formControl = document.querySelector(
-        "select.vanilla__form-control"
-      ) as HTMLElement;
-      formControl.addEventListener("focusout", function (e) {
-        // depending on the target, we should extract the value and update the placeholder
-        const target = e.target as HTMLElement;
-        // if the target is a select, we should extract the value from the selected option
-        let value = "";
-        if (target.tagName === "SELECT") {
-          const select = target as HTMLSelectElement;
-          const option = select.options[select.selectedIndex];
-          value = option.value;
-          console.log(value);
-        }
-        // if the target is an input, we should extract the value from the input
-        if (target.tagName === "INPUT") {
-          const input = target as HTMLInputElement;
-          value = input.value;
-          console.log(value);
-        }
-        // if the target is a textarea, we should extract the value from the textarea
-        if (target.tagName === "TEXTAREA") {
-          const textarea = target as HTMLTextAreaElement;
-          value = textarea.value;
-          console.log(value);
-        }
-        // // if the target is a checkbox, we should extract the value from the checkbox
-        // if (target.tagName === "INPUT" && target.type === "checkbox") {
-        //   const checkbox = target as HTMLInputElement;
-        //   value = checkbox.checked.toString();
-        //   console.log(value);
-        // }
-        // // if the target is a radio, we should extract the value from the radio
-        // if (target.tagName === "INPUT" && target.type === "radio") {
-        //   const radio = target as HTMLInputElement;
-        //   value = radio.value;
-        //   console.log(value);
-        // }
-        // TODO: support other form controls for example file, date, time, etc
+      };
+      const formControls = document.querySelectorAll(
+        ".vanilla__form-control"
+      ) as NodeListOf<HTMLElement>;
+
+      formControls.forEach((control) => {
+        control.addEventListener("focusout", function (e) {
+          const target = e.target as HTMLElement;
+          let value = "";
+          if (target.tagName === "SELECT") {
+            const select = target as HTMLSelectElement;
+            const option = select.options[select.selectedIndex];
+            value = option.value;
+            console.log("select:", value);
+          } else if (target.tagName === "INPUT") {
+            const input = target as HTMLInputElement;
+            console.log("input:", input.type, input.value);
+          } else if (target.tagName === "TEXTAREA") {
+            const textarea = target as HTMLTextAreaElement;
+            value = textarea.value;
+            console.log("text area:", value);
+          }
+          // TODO: support other form controls for example file, date, time, etc
+        });
       });
     });
   });
