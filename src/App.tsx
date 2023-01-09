49d619c0b8e9e8a44dd40ab8f0b1c21d35081978
Abiodun Azeez <iamhabbeboy@gmail.com>
2023-01-09 11:16:46 +0100

    Add quickflow page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import VanillaEditor from "./components/editors/vanilla";
 import { Storybook } from "./components/storybook";
 import HomePage from './components/HomePage';
 import Error404Page from "./components/Error404Page";
+import Quickflow from "./pages/quickflow";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="editor" element={<VanillaEditor />} />
+        <Route path="quickflow" element={<Quickflow />} />
         <Route path="story" element={<Storybook />} />
         <Route path="*" element={<Error404Page />} />
       </Routes>
---
+++ src/pages/quickflow.tsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import QuickflowComponent from "../components/editors/vanilla/components/quickflow";
+
+function Quickflow() {
+  return (
+    <div className="wrapper">
+      <QuickflowComponent />
+    </div>
+  );
+}
+
+export default Quickflow;
