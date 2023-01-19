d6734074b7d5d5f3529dec3cda1f981d6fe728fe
dotunpeters <oyelakindotun@gmail.com>
2023-01-19 20:53:49 +0100

    clear git cache

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# writeonce-app
--- src/components/editors/vanilla/components/sidepanel/index.tsx
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-export default function SidePanel({ component }: { component: JSX.Element }) {
-  return (
-    <div className="vanilla__sidepanel" style={{height: window.innerHeight}}>
-        {component}
-    </div>
-  );
-}
