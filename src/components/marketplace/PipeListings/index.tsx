ae67493a8a62fddc41893a57cc5fd977740b48d3
dotunpeters <oyelakindotun@gmail.com>
2023-01-21 03:01:48 +0100

    remove pipe from pipe names

--- src/components/marketplace/PipeListings/index.tsx
+++ src/components/marketplace/PipeListings/index.tsx
@@ -7,37 +7,37 @@ export default function PipeListings(props: { displayPipeDetails: (sp: {imageUrl
 
     const emailPipe = {
         imageUrl: "https://cdn-icons-png.flaticon.com/512/6052/6052250.png",
-        title: "Email Pipe",
+        title: "Email",
         summary: <p>This pipe allows users to send and <br/>receive electronic messages, <br/>commonly referred to as emails. <br/>These apps can be accessed on a <br/> variety of devices, including <br/> smartphones, tablets, and computers.</p>
     }
 
     const smsPipe = {
         imageUrl: "https://cdn-icons-png.flaticon.com/512/3616/3616112.png",
-        title: "SMS Pipe",
+        title: "SMS",
         summary: <p>This pipe allows users to send and <br/> electronic short messages, <br/>commonly referred to as SMS. <br/>These apps can be accessed on a <br/> variety of devices, including <br/> smartphones, tablets, and computers.</p>
     }
 
     const slackPipe = {
         imageUrl: "https://cdn-icons-png.flaticon.com/512/3800/3800024.png",
-        title: "Slack Pipe",
+        title: "Slack",
         summary: <p>This pipe allows users to send and <br/>receive electronic messages via slack, <br/>commonly referred to as slack messages. <br/>These apps can be accessed on a <br/> variety of devices, including <br/> smartphones, tablets, and computers.</p>
     }
 
     const teamsPipe = {
         imageUrl: "https://cdn-icons-png.flaticon.com/512/906/906349.png",
-        title: "Teams Pipe",
+        title: "Teams",
         summary: <p>This pipe allows users to send and <br/>receive electronic messages, <br/>commonly referred to as emails. <br/>These apps can be accessed on a <br/> variety of devices, including <br/> smartphones, tablets, and computers.</p>
     }
 
     const pdfPipe = {
         imageUrl: "https://cdn-icons-png.flaticon.com/512/4726/4726010.png",
-        title: "PDF Pipe",
+        title: "PDF",
         summary: <p>This pipe allows users to send and <br/>receive electronic messages, <br/>commonly referred to as emails. <br/>These apps can be accessed on a <br/> variety of devices, including <br/> smartphones, tablets, and computers.</p>
     }
 
     const invoicePipe = {
         imageUrl: "https://cdn-icons-png.flaticon.com/512/951/951764.png",
-        title: "Invoice Pipe",
+        title: "Invoice",
         summary: <p>This pipe allows users to send and <br/>receive electronic messages, <br/>commonly referred to as emails. <br/>These apps can be accessed on a <br/> variety of devices, including <br/> smartphones, tablets, and computers.</p>
     }
 
