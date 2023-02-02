4663d317c6a883b76fb7ae5f69947b1ce7067a32
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-02-02 14:21:50 +0100

    cleanup of 'any' type in the codebase

--- src/custom-events/index.ts
+++ src/custom-events/index.ts
@@ -1,9 +1,14 @@
 export const CustomEvents = {
-    PlaceholderAdded: 'placeholder-added',
-    RerenderFloatingToolbar: 'rerender-floating-toolbar',
+  PlaceholderAdded: "placeholder-added",
+  RerenderFloatingToolbar: "rerender-floating-toolbar",
 };
 
-export const dispatchCustomEvent = (eventName: string, detail: any) => {
-    const event = new CustomEvent(eventName, { detail });
-    document.dispatchEvent(event);
-}
+export type CustomEventDetailType = { id: string; name: string };
+
+export const dispatchCustomEvent = (
+  eventName: string,
+  detail: CustomEventDetailType
+) => {
+  const event = new CustomEvent(eventName, { detail });
+  document.dispatchEvent(event);
+};
