0c3097a0bcdb5d13ee3f5f38deb12324fe8d12be
theterminalguy <damiansimonpeter@gmail.com>
2023-02-05 13:33:21 -0500

    Travese the dom

--- src/controllers/editors/vanilla_editor_controller.js
+++ src/controllers/editors/vanilla_editor_controller.js
@@ -9,7 +9,7 @@ export default class VanillaEditorController extends Controller {
   static values = {
     maxChar: {
       type: Number,
-      default: 35 // approximately 500 words
+      default: 35 // approx 300 words
     }
   };
 
@@ -47,42 +47,27 @@ export default class VanillaEditorController extends Controller {
       this.charCountTarget.innerText = `${currentCharCount}/${this.maxCharValue} characters`;
 
       if (currentCharCount > this.maxCharValue) {
-        const htmlWithinLimit = this.contentTarget.innerHTML.slice(0, this.maxCharValue);
-        const plainTextWithinLimit = this.contentTarget.innerText.slice(0, this.maxCharValue);
+        // the goal is to split the text into two fragments without loosing the formatting
+        // the first fragment is what we we will save in the redux store
+        // the second fragment will be surrounded by a red div and will be appended to the end of the text
 
-        contentHTML = htmlWithinLimit
-        contentText = plainTextWithinLimit
+        // go through exactly maxCharValue characters
+        // when we are at the last character, we keep track of the next node
 
-        // ge the tag surrounding the plain text
-        const tagRegex = /<[^>]*>/g;
-        const tags = htmlWithinLimit.match(tagRegex);
-        this.logger.log('TAGS', tags);
+        let nodeCharCount = 0;
+        // let nextNode = null;
+        let prevNode = null;
+        let currentNode = this.contentTarget.firstChild;
 
-        /**
-         * the approach above is bad because we end up with a broken html
-         * we need to find a way to slice the html to the end of the word
-         * meaning we only slice valid html tags
-         * 
-         * this is a good start: https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags/1732454#1732454
-         */
+        while (currentNode && nodeCharCount < this.maxCharValue) {
+          nodeCharCount += currentNode.textContent.length;
+          prevNode = currentNode;
+          currentNode = currentNode.nextSibling;
+        }
 
+        this.logger.log('currentNode', currentNode);
+        this.logger.log('prevNode', prevNode);
 
-        /**
-         * a clever way to solve this problem is to focus on the plain text
-         * and then use the plain text to slice the html
-         * 
-         * - get the plain text within the limit
-         * - find it's surrounding html tags
-         * - slice the html to the end of the tags. If the plain text is in the middle of a tag, we need to close the tag or fix it
-         * - append the redified html to the end of the html within the limit
-         * 
-         */
-
-        // htmlBeyonndLimit select all the text over the limit, slice to end of word
-        const htmlBeyondLimit = this.contentTarget.innerHTML.slice(this.maxCharValue, currentCharCount);
-        this.logger.log('HTML', htmlWithinLimit);
-        this.logger.log('TEXT', plainTextWithinLimit);
-        // this.contentTarget.innerHTML = htmlWithinLimit + this.redifyFragment(htmlBeyondLimit);
       }
       store.dispatch(
         setTemplateContent({
