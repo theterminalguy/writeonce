82f7433150386c73854da437306047b9f2bb5134
fiyintheslim <fiyinoyekunle@gmail.com>
2023-02-16 15:20:29 +0100

    placeholder info count

--- src/components/UI/TemplatePlaceholderInfo/index.tsx
+++ src/components/UI/TemplatePlaceholderInfo/index.tsx
@@ -1,15 +1,44 @@
 import React from 'react'
 import { AiOutlineMail } from "react-icons/ai"
+import { VscBracketDot } from "react-icons/vsc"
+import { BsInputCursorText } from "react-icons/bs"
+import { SlPhone } from "react-icons/sl"
+import { TbNumbers } from "react-icons/tb"
+import { IoCalendarNumberOutline } from "react-icons/io5"
 import "./templatePlaceholderInfo.css"
 
-export const TemplatePlaceholderInfo = () => {
+interface Props {
+    type: string;
+    title: string;
+    count: number
+}
+
+export const TemplatePlaceholderInfo: React.FC<Props> = ({ type, title, count }) => {
     return (
         <div className="vanilla__placeholder-info">
-            <span className="">
-                <span className=""></span>
-                <span className=""></span>
+            <span className="left">
+                <span className="icon"><Icon type={type} /></span>
+                <span className="title">{title}</span>
             </span>
-            <span className=""></span>
+            <span className="right">{count}</span>
         </div>
     )
+}
+
+const Icon: React.FC<{ type: string }> = ({ type }) => {
+    switch (type) {
+        case "email":
+            return <AiOutlineMail />
+        case "text":
+            return <BsInputCursorText />
+        case "number":
+            return <TbNumbers />
+        case "phonenumber":
+            return <SlPhone />
+        case "date":
+            return <IoCalendarNumberOutline />
+        default:
+            return <VscBracketDot />
+    }
+    // return <AiOutlineMail />
 }
