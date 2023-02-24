aafa71075664711d4e8752df79799e55e2361e00
theterminalguy <damiansimonpeter@gmail.com>
2023-02-24 10:31:31 -0500

    manually add client id

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -50,7 +50,7 @@ export default function HomePage(props: {setLogIn: Dispatch<SetStateAction<boole
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             google.accounts.id.initialize({
-                client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+                client_id: '655257869747-6hrcoh04iot27ooe2i9oeju12hvq56q9.apps.googleusercontent.com',
                 callback: handleSignIn,
             })
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
