3ed3db4baaac8208b6057d04ec79a27c460cf5b9
theterminalguy <damiansimonpeter@gmail.com>
2023-02-26 22:59:52 -0500

    WRT-89 add dependabot and google client id

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -51,7 +51,7 @@ export default function HomePage(props: { setLogIn: Dispatch<SetStateAction<bool
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       google.accounts.id.initialize({
-        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        client_id: "551688131939-fddl428ps49ds8s9tb19kf039v5jlqd2.apps.googleusercontent.com",
         callback: handleSignIn,
       })
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
